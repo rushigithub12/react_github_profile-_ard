9ad22b501e714aee0bc8acff0b08e770839d420a
RUSHI <rushikeshkamble9256@gmail.com>
2022-12-17 23:44:43 +0530

    new project assessment

---
+++ react-github-card/src/App.jsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import GitHubCard from "./components/GitHubCard";
+import api_data from "./api_data.json";
+import Grid from "@mui/material/Grid";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+
+// const URL = "https://api.github.com/users/nwtgck/repos";
+
+function App() {
+  const [val, setVal] = useState("");
+  const [repoName, setReportName] = useState("hiteshsahu");
+  const [repos, setRepos] = useState([]);
+
+  const clickHandler = () => {
+    setReportName(val);
+    setVal("");
+  };
+
+    useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(`https://api.github.com/users/${repoName}/repos?sort=updated`);
+        const data = await response.json();
+        setRepos(data)
+    };
+    getData();
+  }, [repoName]);
+
+  const orderedList = repos.slice().sort((a, b) => b.updated_at.localeCompare(a.updated_at))
+
+
+  return (
+    <div className="App">
+      <Stack
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+        sx={{ mb: 3 }}
+      >
+        <TextField
+          id="outlined-basic"
+          label="search repos"
+          variant="outlined"
+          val={val}
+          onChange={(e) => setVal(e.target.value)}
+        />
+        <Button
+          variant="contained"
+          onClick={clickHandler}
+          sx={{ height: 50, width: 80, backgroundColor: "#00838f", color: "#fff" }}
+        >
+          Click
+        </Button>
+      </Stack>
+      <Typography variant="h4" >{repoName} GitHub Repos</Typography>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
+        {!orderedList
+          ? <h3>Loading...</h3>
+          : orderedList?.map((repo, index) => (
+              <Grid item xs={2} sm={4} md={4} key={index}>
+                <GitHubCard key={repo.id} repo={repo} />
+              </Grid>
+            ))}
+      </Grid>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-github-card/src/components/GitHubCard.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
+import StarIcon from "@mui/icons-material/Star";
+import LanguageIcon from "@mui/icons-material/Language";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import Avatar from '@mui/material/Avatar';
+import { format } from "date-fns"
+
+const GitHubCard = ({ repo }) => {
+  return (
+    <Card sx={{ maxWidth: 345, mt: 5, backgroundColor: "#e8eaf6", color: "#00838f", borderRadius: "10px", boxShadow: "9px 6px 5px 1px #8C8C8C", pl: 2, pr: 2 }}>
+      <Avatar
+        alt={repo.login}
+        src={repo.avatar_url}
+        sx={{ width: 56, height: 56 }}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {repo.name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary" sx={{ color: "#006064", fontWeight: "bold" }} >
+          {repo.description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        {repo.language && <Chip label={repo.language} color="primary" sx={{ mr: 10, backgroundColor: "#00838f", color: "#fff" }} />}
+        <Typography>{repo.stargazers_count.toLocaleString()} stars</Typography>
+      </CardActions>
+      <Typography sx={{ fontSize: 12 }}>
+        Last update: {format(new Date(repo.updated_at), "dd MMMM yyyy")}
+      </Typography>
+    </Card>
+  );
+};
+
+export default GitHubCard;
